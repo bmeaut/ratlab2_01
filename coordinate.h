94787f39003f2275adf6bc74cc7a67f37af7362d
Judit Acs <judit@sch.bme.hu>
2020-02-19 09:00:53 +0100

    add missing definitions

--- coordinate.h
+++ coordinate.h
@@ -1,9 +1,14 @@
 #ifndef COORDINATE_H
 #define COORDINATE_H
 
+#include <string>
+#include <iostring>
+
 class Coordinate {
 public:
     double x, y, z;
     Coordinate(double x=0, double y=0, double z=0): x{x}, y{y}, z{z} {}
+    Coordinate(const std::string&);
+    friend std::ostream& operator<<(std::ostream& os, const Coordinate& c);
 }; 
 #endif /* !COORDINATE_H */
