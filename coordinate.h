d04fd621d2dcfa4dcdd289d0aad7d5f1a227b9a0
Judit Acs <judit@sch.bme.hu>
2020-02-19 22:42:01 +0100

    add parameter name

--- coordinate.h
+++ coordinate.h
@@ -8,7 +8,7 @@ class Coordinate {
 public:
     double x, y, z;
     Coordinate(double x=0, double y=0, double z=0): x{x}, y{y}, z{z} {}
-    Coordinate(const std::string&);
+    Coordinate(const std::string& line);
     friend std::ostream& operator<<(std::ostream& os, const Coordinate& c);
 }; 
 #endif /* !COORDINATE_H */
